453a4816780eaba87bff67af495b07d67c2579e1
Matthew Bender <benderm@gmail.com>
2015-12-06 16:42:20 -0700

    KmeansCBI

---
+++ scripts/kmeansCBI.R
@@ -0,0 +1,73 @@
+library(dplyr)
+library(aspace)
+library (data.table)
+library(readr)
+library(fpc)
+
+set.seed(1337)
+
+AVG_EARTH_RADIUS = 6371
+
+haversine <- function(lat1,lng1,lat2,lng2, miles=FALSE){
+  lat1=as_radians(lat1)
+  lat2=as_radians(lat2)
+  lng1=as_radians(lng1)
+  lng2=as_radians(lng2)
+
+  lat = lat2 - lat1
+  lng = lng2 - lng1
+  d = sin(lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(lng / 2) ** 2
+  h = 2 * AVG_EARTH_RADIUS * asin(sqrt(d))
+  if(miles==TRUE) return (h * 0.621371)
+  if(miles==FALSE) return (h)
+}
+
+base_data <- read.csv("../input/gifts.csv")
+##calculates total distance for each trip
+##function input - vector of gift numbers
+weighted_trip_length <- function(trip_gifts) {
+  x=data.frame(Latitude=90,Longitude=0,Weight=0,r=1)
+  x=rbind(x,data.frame(base_data[trip_gifts,2:4],r=2:(length(trip_gifts)+1)))
+  x=rbind(x,data.frame(Latitude=90,Longitude=0,Weight=0,r=length(trip_gifts)+2))
+  x=x %>%
+    arrange(desc(r)) %>%
+    mutate(TotalWeight=cumsum(Weight)+10) %>%
+    arrange(r) %>%
+    mutate(Latitude_next=lead(Latitude),Longitude_next=lead(Longitude),W=lead(TotalWeight)) %>%
+    mutate(Distance=haversine(Latitude,Longitude,Latitude_next,Longitude_next)) %>%
+    na.omit()
+  x=sum(x$Distance*x$W)
+  x
+}
+
+clstrs = 5280
+gifts <- fread("../input/gifts.csv")
+model <- kmeansCBI(gifts[, 2:3, with=FALSE], k=clstrs, iter.max = 10000, 
+                   scaling=TRUE)
+gifts[, TripId := model$partition]
+
+giftcounts <- gifts[, .N, by=TripId]
+solos <- giftcounts[N==1, TripId]
+sapply(solos, function(solo) gifts[TripId==solo, TripId := clstrs+1])
+
+#duos <- giftcounts[N==2, TripId]
+#sapply(duos, function(duos) gifts[TripId==duos, TripId := clstrs+2])
+
+setorder(gifts, -Weight)
+submission <- as.data.frame(gifts[, .(GiftId, TripId)])
+
+##calculate all trip distances
+weightedDistances <- data.frame()
+dist=0.0
+for (i in unique(submission$TripId)) {
+  weightedDist = weighted_trip_length(submission$GiftId[submission$TripId==i])
+  weightedDistances <- rbind(weightedDistances, data.frame(TripID= i,WD=weightedDist))
+  dist = dist + weightedDist
+}
+print(dist)
+
+trips <- merge(base_data, submission)
+tripSums <- aggregate(Weight~TripId, data=trips, FUN=sum)
+nrow(tripSums[tripSums$Weight > 990, ])
+
+write.csv(submission,file="../submissions/kmeansCBI_5280_1000.csv",row.names=FALSE)
